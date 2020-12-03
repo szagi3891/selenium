6aecefa5cf7e7dadc15fe6408dc0c9ac02dc6eca
Grzegorz Szeliga <grzegorz.szeliga@twoupdigital.com>
2020-12-03 22:39:30 +0100

    testy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use thirtyfour::prelude::*;
 use tokio;
 // use tokio::prelude::*;
-
+use std::time::Duration;
 
 /*
 
 https://dev.to/stevepryde/using-selenium-with-rust-aca
+
+
+https://mkyong.com/java/how-to-install-java-on-mac-osx/
+brew tap adoptopenjdk/openjdk
+brew search jdk
+brew cask install adoptopenjdk11
+
+java -jar selenium-server-standalone-3.141.59.jar
+
+
+CHROME DRIVER - uruchamianie z internetu
+https://support.apple.com/pl-pl/guide/mac-help/mh40616/mac
 
 
 https://medium.com/@HoussemDellai/run-selenium-ui-tests-in-docker-containers-f41ae2796b8d
@@ -31,10 +43,19 @@ async fn main() {
     driver.get("https://wikipedia.org/").await.unwrap();
     //https://www.wikipedia.org/
 
-    println!("aaa4");
+    println!("aaa4 - 1");
 
-    let elem_search = driver.find_element(By::Name("div")).await.unwrap();
+    tokio::time::delay_for(Duration::from_secs(5)).await;
 
+    println!("aaa4 - 2");
+
+    //let elem_search = driver.find_element(By::Name("h1")).await.unwrap();
+
+    let elem_search = driver.find_element(By::Id("searchInput")).await.unwrap();
+    elem_search.send_keys("selenium").await.unwrap();
+
+    tokio::time::delay_for(Duration::from_secs(5)).await;
+    
     println!("aaa5");
 
     let ggg = elem_search.inner_html().await.unwrap();
